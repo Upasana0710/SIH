fd6dce9c92115be625ee656ac447bd3938fccbf9
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-09-23 00:40:48 +0530

    made auth apis

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import userRoutes from './src/routes/user.js';
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3300;
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>app.listen(PORT, ()=> {
+        console.log(`Server is running on port ${PORT}`);
+    }))
+    .catch((err)=>console.log(err))
+
+const corsConfig={
+    credentials: "true",
+    origin: "http://localhost:3000",
+    optionSuccessStatus: "200",
+};
+
+app.use(compression());
+app.use(cors(corsConfig));
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+app.use('/user',userRoutes);
---
+++ server/src/models/user.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    instituteEmail: {
+        type: String,
+    },
+    dob: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    programme: {
+        type: String,
+        required: true
+    },
+    branch: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model("User", userSchema);
