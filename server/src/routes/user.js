e0bf8a0219f2e34d97906b03c16fe06fef259dd8
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-12-20 05:22:46 +0530

    made several backend changes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {register, login, addSubjects, getUser, searchUser} from '../controllers/user.js';
+import {register, login, addSubjects, getUser, searchUser, createSlots, getTeacherSlots, updateTeachRating} from '../controllers/user.js';
 import {auth} from '../middleware/auth.js';
 
 const router = express.Router();
@@ -9,5 +9,8 @@ router.post('/login', login);
 router.patch('/addInfo', [auth], addSubjects);
 router.get('/getUser/:id', getUser);
 router.get('/search', [auth], searchUser);
+router.patch('/createSlots', [auth], createSlots);
+router.post('/display', [auth], getTeacherSlots);
+router.patch('/rating', [auth], updateTeachRating);
 
 export default router;
