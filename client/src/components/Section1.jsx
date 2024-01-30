933bba15ea94feb38e21251d7edccc57967580a4
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-01-30 20:47:43 +0530

    added ux for login and logout

--- client/src/components/Section1.jsx
+++ client/src/components/Section1.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import letlogo from "../assets/letlogo.png";
-import AnimatedPage from "./AnimatedPage";
+import React from 'react';
+import letlogo from '../assets/letlogo.png';
+import AnimatedPage from './AnimatedPage';
 
-import styles from "./Section1.module.css";
+import { useSelector } from 'react-redux';
+
+import styles from './Section1.module.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Section1() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  let btn_content = currentUser ? 'GO TO HOME →' : 'GET STARTED →';
+
+  const handleBtnLanding = () => {
+    if (currentUser) {
+      navigate('/home');
+    } else {
+      navigate('/login');
+    }
+  };
   return (
     <AnimatedPage>
       <section className={styles.hero_section}>
@@ -20,6 +35,11 @@ export default function Section1() {
             comprehensive solution, catering to all your educational needs in
             one convenient place
           </h3>
+          <div className={styles.hero_btn_container}>
+            <button className={styles.hero_btn} onClick={handleBtnLanding}>
+              {btn_content}
+            </button>
+          </div>
         </div>
         <div className={styles.hero_img_container}>
           <img id="hero_image" src={letlogo} alt="let-logo" />
