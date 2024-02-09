eebd6d9f5b57dd3a7e8f06cafa6fb2689ef94785
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-02-10 02:01:02 +0530

    changed profile page ui

--- client/src/components/profile/ProfileCard/ProfileCard.jsx
+++ client/src/components/profile/ProfileCard/ProfileCard.jsx
@@ -9,38 +9,38 @@ function ProfileCard({ user }) {
   return (
     <>
       <div className={styles.card_container}>
-        <h3 className={styles.user_name}>{user.name}</h3>
-        <h6 className={styles.user_email}>{user.email}</h6>
-        <h5 className={styles.user_branch}>{user.branch}</h5>
-        <p className={styles.user_bio}>
-          Fueled by a passion for Learning,
-          <br />
-          I&apos;m an Engineering student{' '}
-        </p>
-        <div className={styles.buttons}></div>
-        <div className={styles.skills}>
-          <div className={styles.skills2}>
-            <li className={styles.heading}>
-              My expertise
-              <ul className={styles.courses}>
-                {teachSub.map((course, index) => (
-                  <li className={styles.course_list} key={index}>
-                    {course}
-                  </li>
-                ))}
-              </ul>
-            </li>
-            <li className={styles.vertical_line}></li>
-            <li className={styles.heading}>
-              Looking for
-              <ul className={styles.courses}>
-                {studySub.map((course, index) => (
-                  <li className={styles.course_list} key={index}>
-                    {course}
-                  </li>
-                ))}
-              </ul>
-            </li>
+        <div className={styles.user_text}>
+          <h3 className={styles.user_name}>{user.name}</h3>
+          <h6 className={styles.user_email}>{user.email}</h6>
+          <h5 className={styles.user_branch}>{user.branch}</h5>
+          <p className={styles.user_bio}>
+            Fueled by a passion for Learning,
+            <br />
+            I&apos;m an Engineering student{' '}
+          </p>
+        </div>
+
+        <div className={styles.skills_container}>
+          <div className={styles.skills}>
+            <h3 className={styles.heading}>My Skills</h3>
+            <ul className={styles.courses}>
+              {teachSub.map((course, index) => (
+                <li className={styles.course_list} key={index}>
+                  {course}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.vertical_line}></div>
+          <div className={styles.skills}>
+            <h3 className={styles.heading}>Looking for</h3>
+            <ul className={styles.courses}>
+              {studySub.map((course, index) => (
+                <li className={styles.course_list} key={index}>
+                  {course}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
