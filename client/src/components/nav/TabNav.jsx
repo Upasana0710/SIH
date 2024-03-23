26d63d45aa1fb76799a06b3ec5c5e48919c12761
xaliz-06 <vaibhavchakraborty06@gmail.com>
2024-03-24 00:30:11 +0530

    delete sidebar communities

--- client/src/components/nav/TabNav.jsx
+++ client/src/components/nav/TabNav.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 import Break from "../../ui/Break";
 
 import styles from "./TabNav.module.css";
 
 const TabNav = () => {
   const tabs = [
-    { name: "Home Feed", id: "#1", to: " " },
+    { name: "Home Feed", id: "#1", to: "/home/" },
     { name: "Latest", id: "#2", to: "/home/latest" },
     { name: "Hot", id: "#3", to: "/home/hot" },
     { name: "Discover", id: "#4", to: "/home/discover" },
     { name: "Your Communities", id: "#5", to: "/home/communities" },
   ];
 
-  const [tabSelect, setTabSelect] = useState(tabs[0].id);
+  const location = useLocation().pathname;
+
+  const [tabSelect, setTabSelect] = useState(location);
 
   const tabs_section = (
     <nav className={styles.tab_nav}>
@@ -22,9 +24,9 @@ const TabNav = () => {
           {tabs.map((tab) => (
             <NavLink
               to={tab.to}
-              className={tabSelect === tab.id ? styles.selected : undefined}
+              className={tabSelect === tab.to ? styles.selected : undefined}
               key={tab.id}
-              onClick={() => changeTabHandler(tab.id)}
+              onClick={() => changeTabHandler(tab.to)}
             >
               {tab.name}
             </NavLink>
--- client/src/components/right-sidebar/Sidebar.jsx
+++ client/src/components/right-sidebar/Sidebar.jsx
@@ -3,15 +3,12 @@ import React from "react";
 import styles from "./Sidebar.module.css";
 import Actions from "./sidebar_actions/Actions";
 import Break from "../../ui/Break";
-import Communities from "./sidebar_communities/Communities";
 
 const Sidebar = () => {
   return (
     <React.Fragment>
       <div className={styles.sidebar}>
         <Actions />
-        <Break />
-        <Communities />
         <Break />
         <div className={styles.sidebar_footer}>
           <p className={styles.text}>
